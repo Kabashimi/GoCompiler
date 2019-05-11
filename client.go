0d560e9071cb49d4497f1ad988324f2794886d11
Johny_dudek <johny.dudek@gmail.com>
2019-05-11 20:44:15 +0200

    Client side cleanup

--- client.go
+++ client.go
@@ -1,7 +1,6 @@
 package main
 
 import (
-	"bytes"
 	"flag"
 	"fmt"
 	"github.com/gorilla/websocket"
@@ -10,22 +9,12 @@ import (
 	"net/url"
 	"os"
 	"os/signal"
-	"strings"
 	"time"
-	"strconv"
 )
 
 var addr = flag.String("addr", "192.168.0.17:8080", "http service address")
 
 func main() {
-	a:= "package main\nimport \"log\"\nfunc mainDUPA() {\nlog.Printf(\"dupa\")\n}"
-
-	b:= "package main\nimport \"log\"\nfunc main() {\nlog.Printf(\"dupa\")\n}"
-
-	text, liczba :=Diff(a,b)
-
-	fmt.Println(text)
-	fmt.Println(string(liczba))
 
 	file_to_send, err := ioutil.ReadFile("data.go")
 
@@ -60,7 +49,7 @@ func main() {
 				return
 			}
 			mes = string(message)
-			log.Printf("recv: %s", message)
+			//log.Printf("recv: %s", message)
 			//awaiting first message
 			if !awaitingResults {
 				//compile success/failure info handle:
@@ -111,164 +100,3 @@ func main() {
 }
 
 
-func Diff(A, B string) (string, int) {
-	aLines := strings.Split(A, "\n")
-	bLines := strings.Split(B, "\n")
-
-	chunks := DiffChunks(aLines, bLines)
-	equalSum := 1
-	addedSum := 0
-	deletedSum := 0
-
-	buf := new(bytes.Buffer)
-	for _, c := range chunks {
-		for _, line := range c.Added {
-			addedSum += 1
-			fmt.Fprintf(buf, "+%s\n", line)
-		}
-		for _, line := range c.Deleted {
-			deletedSum += 1
-			fmt.Fprintf(buf, "-%s\n", line)
-		}
-		for _, line := range c.Equal {
-			equalSum += 1
-			fmt.Fprintf(buf, " %s\n", line)
-		}
-	}
-	fmt.Println("Equal = "+strconv.Itoa(equalSum))
-	//equalPercent := (equalSum * 100) / (equalSum + addedSum + deletedSum)
-	return strings.TrimRight(buf.String(), "\n"), 100
-}
-
-func DiffChunks(a, b []string) []Chunk {
-	// algorithm: http://www.xmailserver.org/diff2.pdf
-
-	// We'll need these quantities a lot.
-	alen, blen := len(a), len(b) // M, N
-
-	// At most, it will require len(a) deletions and len(b) additions
-	// to transform a into b.
-	maxPath := alen + blen // MAX
-	if maxPath == 0 {
-		// degenerate case: two empty lists are the same
-		return nil
-	}
-
-	// Store the endpoint of the path for diagonals.
-	// We store only the a index, because the b index on any diagonal
-	// (which we know during the loop below) is aidx-diag.
-	// endpoint[maxPath] represents the 0 diagonal.
-	//
-	// Stated differently:
-	// endpoint[d] contains the aidx of a furthest reaching path in diagonal d
-	endpoint := make([]int, 2*maxPath+1) // V
-
-	saved := make([][]int, 0, 8) // Vs
-	save := func() {
-		dup := make([]int, len(endpoint))
-		copy(dup, endpoint)
-		saved = append(saved, dup)
-	}
-
-	var editDistance int // D
-dLoop:
-	for editDistance = 0; editDistance <= maxPath; editDistance++ {
-		// The 0 diag(onal) represents equality of a and b.  Each diagonal to
-		// the left is numbered one lower, to the right is one higher, from
-		// -alen to +blen.  Negative diagonals favor differences from a,
-		// positive diagonals favor differences from b.  The edit distance to a
-		// diagonal d cannot be shorter than d itself.
-		//
-		// The iterations of this loop cover either odds or evens, but not both,
-		// If odd indices are inputs, even indices are outputs and vice versa.
-		for diag := -editDistance; diag <= editDistance; diag += 2 { // k
-			var aidx int // x
-			switch {
-			case diag == -editDistance:
-				// This is a new diagonal; copy from previous iter
-				aidx = endpoint[maxPath-editDistance+1] + 0
-			case diag == editDistance:
-				// This is a new diagonal; copy from previous iter
-				aidx = endpoint[maxPath+editDistance-1] + 1
-			case endpoint[maxPath+diag+1] > endpoint[maxPath+diag-1]:
-				// diagonal d+1 was farther along, so use that
-				aidx = endpoint[maxPath+diag+1] + 0
-			default:
-				// diagonal d-1 was farther (or the same), so use that
-				aidx = endpoint[maxPath+diag-1] + 1
-			}
-			// On diagonal d, we can compute bidx from aidx.
-			bidx := aidx - diag // y
-			// See how far we can go on this diagonal before we find a difference.
-			for aidx < alen && bidx < blen && a[aidx] == b[bidx] {
-				aidx++
-				bidx++
-			}
-			// Store the end of the current edit chain.
-			endpoint[maxPath+diag] = aidx
-			// If we've found the end of both inputs, we're done!
-			if aidx >= alen && bidx >= blen {
-				save() // save the final path
-				break dLoop
-			}
-		}
-		save() // save the current path
-	}
-	if editDistance == 0 {
-		return nil
-	}
-	chunks := make([]Chunk, editDistance+1)
-
-	x, y := alen, blen
-	for d := editDistance; d > 0; d-- {
-		endpoint := saved[d]
-		diag := x - y
-		insert := diag == -d || (diag != d && endpoint[maxPath+diag-1] < endpoint[maxPath+diag+1])
-
-		x1 := endpoint[maxPath+diag]
-		var x0, xM, kk int
-		if insert {
-			kk = diag + 1
-			x0 = endpoint[maxPath+kk]
-			xM = x0
-		} else {
-			kk = diag - 1
-			x0 = endpoint[maxPath+kk]
-			xM = x0 + 1
-		}
-		y0 := x0 - kk
-
-		var c Chunk
-		if insert {
-			c.Added = b[y0:][:1]
-		} else {
-			c.Deleted = a[x0:][:1]
-		}
-		if xM < x1 {
-			c.Equal = a[xM:][:x1-xM]
-		}
-
-		x, y = x0, y0
-		chunks[d] = c
-	}
-	if x > 0 {
-		chunks[0].Equal = a[:x]
-	}
-	if chunks[0].empty() {
-		chunks = chunks[1:]
-	}
-	if len(chunks) == 0 {
-		return nil
-	}
-	return chunks
-}
-
-type Chunk struct {
-	Added   []string
-	Deleted []string
-	Equal   []string
-}
-
-func (c *Chunk) empty() bool {
-	return len(c.Added) == 0 && len(c.Deleted) == 0 && len(c.Equal) == 0
-}
