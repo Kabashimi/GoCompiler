7496613c75933e5e9210c53bb663837d0a37cebd
truskawkowamalina <balamutmarcelina@gmail.com>
2019-05-12 13:31:28 +0200

    add server unit tests

--- tests_test.go
+++ tests_test.go
@@ -1,28 +1,36 @@
 package main
 
 import (
-	"fmt"
 	"testing"
 )
 
-func TestCompare(t *testing.T) {
+func TestCompareSameCodes(t *testing.T) {
 
-	firstCode := "package main import " + "fmt" + "func main() { fmt.Println(" + "hello world" + ") }"
-	total, percentage := Diff(firstCode, firstCode)
+	code := "package main import " + "fmt" + "func main() { fmt.Println(" + "hello world" + ") }"
+	_, percentage := Diff(code, code)
+
 	if percentage != 100 {
-		t.Errorf("Sum was incorrect, got: %d, want: %d.", percentage, 100)
-		fmt.Print(total)
+		t.Errorf("Percentage was incorrect, got: %d, want: %d.", percentage, 100)
 	}
 
 }
 
-//func TestCompare2(t *testing.T) {
-//
-//
-//	firstCode := "package main import " +"fmt" +"func main() { fmt.Println("+"hello world"+") }"
-//	total, percentage:= Diff(firstCode, firstCode)
-//	if total != "" {
-//		t.Errorf("Sum was incorrect, got: %d, want: %d.", total, "kk")
-//		fmt.Print(percentage)
-//	}
-//}
+func TestCompareDiffCodes(t *testing.T) {
+	firstCode := "package main \nimport " + "fmt" + "\nfunc main() {\n fmt.Println(" + "hello" + ") \n fmt.Println(" + "hello1" + ")}"
+	secondCode := "package main \nimport " + "fmt" + "\nfunc main() {\n fmt.Println(" + "hello" + ")\n + for j := 7; j <= 9; j++ {fmt.Println(j)\n} }"
+	_, percentage := Diff(firstCode, secondCode)
+	if percentage != 44 {
+		t.Errorf("Percentage was incorrect, got: %d, want: %d.", percentage, 44)
+	}
+
+}
+
+func TestCompareByRaportSameCodes(t *testing.T) {
+
+	code := "package main import " + "fmt" + "func main() { fmt.Println(" + "hello world" + ") }"
+	total, _ := Diff(code, code)
+	if total != "" {
+		t.Errorf("Result was incorrect, got: %s, want: %s.", total, "")
+	}
+
+}
